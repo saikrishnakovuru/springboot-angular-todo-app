35ae89de80e9180b3b5f59ffae203824de954e3b
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2024-03-17 15:32:11 -0500

    Organised imports

--- todo-rest/src/main/java/com/todorest/controllers/HelloWorldController.java
+++ todo-rest/src/main/java/com/todorest/controllers/HelloWorldController.java
@@ -1,17 +1,14 @@
 package com.todorest.controllers;
 
-import com.todorest.entity.HelloWorldBean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.todorest.entity.HelloWorldBean;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
 public class HelloWorldController {
-
-
 
   @GetMapping("/hello")
   public HelloWorldBean sayHello() {
@@ -22,6 +19,5 @@ public class HelloWorldController {
   public HelloWorldBean test() {
     throw new RuntimeException("No such record");
   }
-
 
 }
